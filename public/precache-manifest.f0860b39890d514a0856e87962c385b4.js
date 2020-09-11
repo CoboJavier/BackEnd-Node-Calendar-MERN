d000cb42f2d99bfa6a5da05f68e534fd5c19d839
Javier <fcobo9608@gmail.com>
2020-09-10 21:15:53 -0500

    FrontEnd Login/Registro

---
+++ public/precache-manifest.f0860b39890d514a0856e87962c385b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94313a529d62e00f6c40f7acd7be2587",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9abe211611a362ec06f",
+    "url": "/static/css/2.dabbbe1b.chunk.css"
+  },
+  {
+    "revision": "419ba394489baa93b2ec",
+    "url": "/static/css/main.3d9de7c7.chunk.css"
+  },
+  {
+    "revision": "e9abe211611a362ec06f",
+    "url": "/static/js/2.7309402f.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.7309402f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "419ba394489baa93b2ec",
+    "url": "/static/js/main.63ec2f0a.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
